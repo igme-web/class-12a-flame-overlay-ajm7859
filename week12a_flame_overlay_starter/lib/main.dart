f479b0039024ec3349bf661054f8444971cd3834
Anthony Mullins <92737177+ajm7859@users.noreply.github.com>
2025-11-11 12:11:10 -0500

    Update main.dart

--- week12a_flame_overlay_starter/lib/main.dart
+++ week12a_flame_overlay_starter/lib/main.dart
@@ -1,6 +1,11 @@
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
+import 'overlay_title.dart';
+import 'overlay_main.dart';
+import 'overlay_pause.dart';
+import 'overlay_info.dart';
+import 'overlay_settings.dart';
 
 import 'game.dart';
 
@@ -20,7 +25,26 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: GameWidget(
-          game: OverlayTutorial(context),
+          game: OverlayTutorial(context)
+            ..paused = true, // Cascade to pause immediately
+          overlayBuilderMap: {
+            'title': (context, game) {
+              return OverlayTitle(game: game);
+            },
+            'main': (context, game) {
+              return mainOverlay(context, game);
+            },
+            'pause': (context, game) {
+              return pauseOverlay(context, game);
+            },
+            'info': (context, game) {
+              return InfoOverlay(game: game as OverlayTutorial); // Cast required!
+            },
+            'settings': (context, game) {
+              return settingsOverlay(context, game);
+            },
+          },
+          initialActiveOverlays: const ['title'],
         ),
       ),
     );
